4ae57da09ad7dd31d9ca34e9be96db9b21fc3010
Bing Yang <bingyang@nanometrics.ca>
2021-11-03 14:51:15 -0400

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatCardModule, MatButtonModule } from '@angular/material'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ const materialModules = [
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ...materialModules,
   ],
   providers: [],
